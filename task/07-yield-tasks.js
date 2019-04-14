c909ebf82dfe50f92ba2793c72c3b81416417302
Greenyaaa <mikoruro@list.ru>
2019-04-14 18:44:07 +0300

    feat: yield tasks solutions

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -47,7 +47,12 @@ function* get99BottlesOfBeer() {
  *
  */
 function* getFibonacciSequence() {
-    throw new Error('Not implemented');
+    var fib1 = 0;
+    var fib2 = 1;
+    while (true) {
+        yield fib1;
+        [fib1, fib2] = [fib2, fib1 + fib2];
+    };
 }
 
 
